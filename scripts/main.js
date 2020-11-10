1e9baa0794e37abf5fb7e86c0056c0beb6a65d72
Joseph Burke <joseph_burke@outlook.com>
2020-11-10 12:40:24 +0000

    Add additional styles. Insert removal buttons into cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 let myLibrary = [];
 
+const btn = document.querySelector('#btn');
+const form = document.querySelector('#form1');
+const section = document.querySelector(".book-list")
+
+addBookToLibrary(new Book("Harry Potter", "J.K.Rowling", 652, true));
+addBookToLibrary(new Book("Catcher in the Rye", "J.D. Salinger", 652, true));
+addBookToLibrary(new Book("Crime and Punishment", "Fyodor Dostoevsky", 652, true));
+addBookToLibrary(new Book("Cien Años de Soledad", "Gabriel Marquez", 652, true));
+
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -11,37 +20,29 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+}
+
 function displayLibrary() {
   for (let i = 0 ; i < myLibrary.length ; i++) {
     const bookCard = document.createElement("div")
-    bookCard.classList.add("card");
-
+    
+    bookCard.classList.add("card", "card-body");
     bookCard.textContent = myLibrary[i]["title"] + ", by " + myLibrary[i]["author"]
 
-    const section = document.querySelector(".book-list")
-    section.appendChild(bookCard)
+    // Add JS to put a button in each card
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-primary");
+    deleteButton.textContent = "Remove Book";
 
+    bookCard.appendChild(deleteButton);
+    section.appendChild(bookCard)
   }
 }
-
-addBookToLibrary(new Book("Harry Potter", "J.K.Rowling", 652, true));
-addBookToLibrary(new Book("Catcher in the Rye", "J.D. Salinger", 652, true));
-addBookToLibrary(new Book("Crime and Punishment", "Fyodor Dostoevsky", 652, true));
-addBookToLibrary(new Book("Cien Años de Soledad", "Gabriel Marquez", 652, true));
-
-displayLibrary();
-
-const btn = document.querySelector('#btn');
-const form = document.querySelector('#form1');
-
-// btn.addEventListener('click', form.classList.toggle('btn-secondary'));
-// btn.addEventListener("click", form.classList.toggle("d-none"));
 
 function displayForm() {
   form.classList.toggle("d-none")
 }
 
-function submitForm() {
-  
-
-}
+displayLibrary();

783a70147a1ba00809e80c8680ca90056c0da568
Joseph Burke <joseph_burke@outlook.com>
2020-11-10 08:33:03 +0000

    Add displayLibrary function to add books to page

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,23 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
+
+function displayLibrary() {
+  for (let i = 0 ; i < myLibrary.length ; i++) {
+    const bookCard = document.createElement("div")
+    bookCard.classList.add("card");
+
+    bookCard.textContent = myLibrary[i]["title"] + ", by " + myLibrary[i]["author"]
+
+    const section = document.querySelector(".book-list")
+    section.appendChild(bookCard)
+
+  }
+}
+
+addBookToLibrary(new Book("Harry Potter", "J.K.Rowling", 652, true));
+addBookToLibrary(new Book("Catcher in the Rye", "J.D. Salinger", 652, true));
+addBookToLibrary(new Book("Crime and Punishment", "Fyodor Dostoevsky", 652, true));
+addBookToLibrary(new Book("Cien Años de Soledad", "Gabriel Marquez", 652, true));
+
+displayLibrary();

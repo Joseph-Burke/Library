2029b8b48f2d64834d704a38ae684019d9f95f85
EstebanP <epinczinger@gmail.com>
2020-11-10 12:32:29 -0300

    set up and fix eslint errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,57 @@
-let myLibrary = [];
+const myLibrary = [];
 
-const btn = document.querySelector('#btn');
 const form = document.querySelector('#form1');
-const bookList = document.querySelector(".book-list");
-const inputTitle = document.getElementById('input-title')
-const inputAuthor = document.getElementById("input-author");
-const inputPages = document.getElementById("input-pages");
-const inputRead = document.getElementById("input-read");
-
-addBookToLibrary(new Book("Harry Potter and the Chamber of Secrets", "J.K.Rowling", 533, true));
-addBookToLibrary(new Book("Catcher in the Rye", "J.D. Salinger", 297, true));
-addBookToLibrary(new Book("Crime and Punishment", "Fyodor Dostoevsky", 684, true));
-addBookToLibrary(new Book("Cien Años de Soledad", "Gabriel Marquez", 484, true));
+const bookList = document.querySelector('.book-list');
+const inputTitle = document.getElementById('input-title');
+const inputAuthor = document.getElementById('input-author');
+const inputPages = document.getElementById('input-pages');
+const inputRead = document.getElementById('input-read');
+const btn = document.getElementById('submit-form');
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
 
 function removeBookFromLibrary(index) {
   myLibrary.splice(index, 1);
 }
 
+function readStatus(book) {
+  if (book.read) {
+    return 'You have read this book.';
+  }
+  return 'You have not yet read this book.';
+}
+
 function displayLibrary() {
   // Remove all children of bookList
-  bookList.innerHTML = ""
-  for (let i = 0 ; i < myLibrary.length ; i++) {
-    const bookCard = document.createElement("div")
-    bookCard.classList.add("card", "card-body");
+  bookList.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card', 'card-body');
 
-    let title = document.createElement("h5");
-    title.classList.add("card-title");
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
     title.textContent = `"${myLibrary[i].title}" by ${myLibrary[i].author}`;
 
-    let text = document.createElement("p");
-    text.classList.add("card-text");
+    const text = document.createElement('p');
+    text.classList.add('card-text');
     text.textContent = `This book has ${myLibrary[i].pages} pages. ${readStatus(myLibrary[i])}`;
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("btn", "btn-primary");
-    deleteButton.textContent = "Remove Book";
-    deleteButton.setAttribute("data-attribute", i);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn', 'btn-primary');
+    deleteButton.textContent = 'Remove Book';
+    deleteButton.setAttribute('data-attribute', i);
 
-    deleteButton.addEventListener("click", function(event) {
-      let index = event.target.getAttribute("data-attribute");
+    deleteButton.addEventListener('click', (event) => {
+      const index = event.target.getAttribute('data-attribute');
       removeBookFromLibrary(index);
       displayLibrary();
     });
@@ -57,46 +59,51 @@ function displayLibrary() {
     const readButton = document.createElement('button');
     readButton.classList.add('btn', 'btn-secondary');
     readButton.textContent = 'Change Read Status';
-    readButton.setAttribute("data-attribute", i);
+    readButton.setAttribute('data-attribute', i);
 
-    readButton.addEventListener("click", function(event) {
-      let index = event.target.getAttribute("data-attribute");
-      let book = myLibrary[index];
+    readButton.addEventListener('click', (event) => {
+      const index = event.target.getAttribute('data-attribute');
+      const book = myLibrary[index];
       book.read = !book.read;
       displayLibrary();
     });
-    
+
     bookCard.appendChild(title);
     bookCard.appendChild(text);
     bookCard.appendChild(deleteButton);
     bookCard.appendChild(readButton);
-    bookList.appendChild(bookCard)
+    bookList.appendChild(bookCard);
   }
 }
 
+function displayForm() {
+  form.classList.toggle('d-none');
+}
+
+btn.addEventListener('click', submitForm());
+
 function submitForm() {
   addBookToLibrary(
-    new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked)
+    new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked),
   );
-  inputTitle.value = "";
-  inputAuthor.value = "";
-  inputPages.value = "";
+  inputTitle.value = '';
+  inputAuthor.value = '';
+  inputPages.value = '';
   inputRead.checked = false;
 
   displayForm();
   displayLibrary();
 }
 
-function displayForm() {
-  form.classList.toggle("d-none");
-}
-
-function readStatus(book) {
-  if (book.read) {
-    return "You have read this book."
-  } else {
-    return "You have not yet read this book."
-  }
-}
+addBookToLibrary(
+  new Book('Harry Potter and the Chamber of Secrets', 'J.K.Rowling', 533, true),
+);
+addBookToLibrary(new Book('Catcher in the Rye', 'J.D. Salinger', 297, true));
+addBookToLibrary(
+  new Book('Crime and Punishment', 'Fyodor Dostoevsky', 684, true),
+);
+addBookToLibrary(
+  new Book('Cien Años de Soledad', 'Gabriel Marquez', 484, true),
+);
 
 displayLibrary();
